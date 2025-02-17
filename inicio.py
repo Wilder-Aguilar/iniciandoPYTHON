38615a86abfe7d8406b3124587c28940e1fc5b66
Wilder-Aguilar <aguilarwilder@gmail.com>
2025-02-17 15:11:31 +0100

    add funciones

--- inicio.py
+++ inicio.py
@@ -26,3 +26,15 @@ d = funcion(10,
 
 print ('Hola Mundo!')
 'Hola Mundo!'
+
+import math
+print (math.pi*4)
+print (math.sqrt(4))
+
+import random
+print (random.randint(1,10))
+lista = [1,2,3,4,5]
+print (random.choice(lista))
+
+print (2*3+2*4)
+print (2*(3+2)*4)
