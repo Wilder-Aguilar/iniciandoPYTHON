a8b60f32e3ef229dde4262daf8ae1feee30da1bb
Wilder-Aguilar <aguilarwilder@gmail.com>
2025-02-21 17:05:00 +0100

    feat: first functions

---
+++ funciones.py
@@ -0,0 +1,43 @@
+def suma(a, b):
+    return a+b
+
+print (suma(2,3))
+
+def en_pantalla(frase1, frase2):
+    print (frase1, frase2)
+
+en_pantalla("Hola", "Mundo")
+
+print (suma(2.7,4.0))
+print (suma('Me gusta', 'Python'))
+
+def f1(a):
+    print (a)
+    b=300
+    def f2(x):
+        print (x)
+    f2('Mundo')
+f1('Hola')
+
+#RECURSIVIDAD
+
+def factorial(x):
+    if x>1:
+        return x*factorial(x-1)
+    else:
+        return 1
+
+print (factorial(5))
+
+def maxmin(lista):
+    return max(lista), min(lista)
+
+lista = [5,4,3,2,1,0,13]
+maximo, minimo = maxmin(lista)
+print (maximo, minimo, sep=' ')
+
+def factores(x):
+    if x%2==0: 
+        return x
+x=6
+print (factores(x))
