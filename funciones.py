bb4a4429157b57a1fbc9b77f63106fd24eb1e2fc
Wilder-Aguilar <aguilarwilder@gmail.com>
2025-02-24 10:49:44 +0100

    feat: adding variables

--- funciones.py
+++ funciones.py
@@ -20,7 +20,6 @@ def f1(a):
 f1('Hola')
 
 #RECURSIVIDAD
-
 def factorial(x):
     if x>1:
         return x*factorial(x-1)
@@ -36,8 +35,10 @@ lista = [5,4,3,2,1,0,13]
 maximo, minimo = maxmin(lista)
 print (maximo, minimo, sep=' ')
 
-def factores(x):
-    if x%2==0: 
-        return x
-x=6
-print (factores(x))
+def log(*args):
+  """
+  Imprime los argumentos que se le pasan por pantalla en una misma
+  línea, con el prefijo 'LOG: '.
+  """
+
+  print('LOG:', *args)
---
+++ variables.py
@@ -0,0 +1,18 @@
+x = 15
+y = "Ana"
+print(x)
+print(y)
+
+# Guardamos en la variable suma el resultado de 1 + 2
+suma = 1 + 2
+# Accedemos al resultado de 1 + 2 a través de la variable suma
+print(suma)
+
+# Asigna a la variable <a> el valor 1
+a = 1
+# Asigna a la variable <a> el resultado de la expresión 3 * 4
+b = 3 * 4
+
+c = 'Pythonista'
+print(a, b ,c)
+
