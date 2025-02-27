6811de8cd6b70ca8308c47d6be77ba3938e36fa5
Wilder-Aguilar <aguilarwilder@gmail.com>
2025-02-27 14:58:53 +0100

    feat: adding IF

---
+++ condicionales.py
@@ -0,0 +1,13 @@
+# IF
+a = 10
+b = 10
+
+if a > b:
+    print("A es menor que B")
+elif a == b:
+    print("A es igual a B")
+else:
+    print("A es mayor que B")
+
+print ("Ya estamos fuera del if")
+
